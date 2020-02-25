82e208e6bdb471670bf0e13fcecb396358122668
gisnetzEU <belladonnaying@gmail.com>
2020-02-25 15:58:01 +0100

    teaching-materials-loops

--- teaching-materials/if_else_Statements.js
+++ teaching-materials/if_else_Statements.js
@@ -1,1 +1,49 @@
 // IF/ELSE statements
+function greaterNum(n1,n2){
+if (n1>n2)
+  console.log("The greater number of",n1,"and", n2," is ",n1);
+else
+  console.log("The greater number of",n1,"and", n2," is ",n2);
+}
+greaterNum(5,2);
+greaterNum(1,6);
+
+function helloWorld(lang){
+    if (lang == 'fr') {
+        return 'Bonjour tout le monde';
+    } else if (lang == 'es') {
+        return 'Hola, Mundo';
+    } else {
+        return 'Hello, World';
+    }
+}
+console.log(helloWorld('en'));
+console.log(helloWorld('fr'));
+console.log(helloWorld('es'));
+
+function assignGrade(score){
+    if (score > 90) {
+        return 'A';
+    } else if (score > 80) {
+        return 'B';
+    } else if (score > 70) {
+        return 'C';
+    } else if (score > 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+console.log('You got a ' + assignGrade(95));
+console.log('You got a ' + assignGrade(65));
+
+function pluralize(noun, number){
+    if (number != 1 && noun != 'sheep' && noun != 'geese') {
+        return number + ' ' + noun + 's';
+    } else {
+        return number + ' ' + noun;
+    }
+}
+console.log('I have ' + pluralize('cat', 0));
+console.log('I have ' + pluralize('cat', 1));
+console.log('I have ' + pluralize('cat', 2));
--- teaching-materials/loop.js
+++ teaching-materials/loop.js
@@ -6,7 +6,7 @@ while (x < 5) {
     x = x + 1;
   }
 
-
+//
   var age = 20;
 if (age >= 35) {
   console.log('You can vote AND hold any place in government!');
@@ -17,17 +17,29 @@ if (age >= 35) {
 } else {
   console.log('You have no voice in government!');
 }
-
+//
 
 for (var i = 0; i < 5; i = i + 1) {
     console.log(i);
   }
 
 
-  for (var current = 100; current < 200; current++) {
+//
+for (var current = 100; current < 200; current++) {
     console.log('Testing ' + current);
     if (current % 7 == 0) {
       console.log('Found it! ' + current);
       break;
     }
   }
+
+  for (var i = 1; i <= 20; i++) {
+    if (i%2 == 0)
+    console.log(i , "is even");
+    else console.log(i);
+  }
+  for (var i = 1; i <= 10; i++) {
+    for (var j = 1; j <= 10; j++) {
+    console.log(i,"*",j,"=",i*j);
+  }
+}
